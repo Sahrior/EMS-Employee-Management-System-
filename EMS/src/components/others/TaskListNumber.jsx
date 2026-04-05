f9122f42806ab233cd822665c9a871abe8bcdcad
Md Sahrior Haider Shovon <165468630+Sahrior@users.noreply.github.com>
2026-04-06 01:48:51 +0600

    employee dashboard added

---
+++ EMS/src/components/others/TaskListNumber.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+
+const TaskListNumber = () => {
+  return (
+    <div className='flex flex-wrap mt-10 justify-between gap-6 w-full px-6'>
+
+      <div className='rounded-2xl w-full sm:w-[48%] lg:w-[23%] p-6 bg-red-400 shadow-md hover:scale-105 transition-all duration-300'>
+        <h2 className='text-4xl font-bold text-white'>0</h2>
+        <h3 className='text-lg font-medium text-white mt-2'>New Task</h3>
+      </div>
+
+      <div className='rounded-2xl w-full sm:w-[48%] lg:w-[23%] p-6 bg-blue-400 shadow-md hover:scale-105 transition-all duration-300'>
+        <h2 className='text-4xl font-bold text-white'>0</h2>
+        <h3 className='text-lg font-medium text-white mt-2'>New Task</h3>
+      </div>
+
+      <div className='rounded-2xl w-full sm:w-[48%] lg:w-[23%] p-6 bg-yellow-400 shadow-md hover:scale-105 transition-all duration-300'>
+        <h2 className='text-4xl font-bold text-white'>0</h2>
+        <h3 className='text-lg font-medium text-white mt-2'>New Task</h3>
+      </div>
+
+      <div className='rounded-2xl w-full sm:w-[48%] lg:w-[23%] p-6 bg-green-400 shadow-md hover:scale-105 transition-all duration-300'>
+        <h2 className='text-4xl font-bold text-white'>0</h2>
+        <h3 className='text-lg font-medium text-white mt-2'>New Task</h3>
+      </div>
+
+    </div>
+  )
+}
+
+export default TaskListNumber
